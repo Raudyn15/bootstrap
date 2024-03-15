a70bdae74aa71c29672c207346524e46341c24e0
raudyn <rauddynkl@gmail.com>
2024-03-14 22:52:55 -0600

    versión 1

---
+++ js/Logica.js
@@ -0,0 +1,49 @@
+﻿let nombre = 'Raudyn';
+console.log(nombre);
+
+let numero1 = 12;
+let numero2 = 567;
+let resultado = numero1 + numero2;
+console.log(resultado);
+
+if (numero1 > numero2) {
+    console.log('Numero 1 es mayor a numero 2');
+} else {
+    console.log('Numero 2 es mayor a numero 1');
+}
+
+//Ejemplos de creación de un array
+const listadoNumeros = [456, 23, 668, 745, 125, 63];
+console.log(listadoNumeros[3]);
+
+//Uso del for para recorrer el array
+for (let contador = 0; contador < listadoNumeros.length; contador++) {
+    console.log('El valor del array en la posición ' + contador + ' es: ' +  listadoNumeros[contador]);
+}
+
+//Uso de un array de manera dinámica
+const listadoPersonas = [];
+listadoPersonas.push('Raudyn');
+listadoPersonas.push('Elena');
+listadoPersonas.push('Michi');
+listadoPersonas.push('enner');
+listadoPersonas.push('Sil');
+
+for (let contador = 0; contador < listadoPersonas.length; contador++) {
+
+    if (listadoPersonas[contador] == 'Eduardo') {
+        console.log('Esta en la lista');
+    }else {
+        console.log('No esta en la lista');
+    }
+}
+
+//Validación == y ===
+
+let x = 10;
+let y = '10';
+
+if (x === y) {
+    console.log(' No son iguales');
+}
+
---
+++ js/UsoDOM.js
@@ -0,0 +1,51 @@
+﻿function Sumar(){
+    let num1 = document.getElementById('numero1').value;
+    let num2 = document.getElementById('numero2').value;
+
+    let calculo =  parseInt(num1) + parseInt(num2);
+
+    const resultado = document.getElementById('resultado');
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    //Setear el valor
+    resultado.innerHTML = calculo;
+
+    
+}
+
+function Restar() {
+    let limpiar = '';
+    let num1 = document.getElementById('numero1').value;
+    let num2 = document.getElementById('numero2').value;
+
+    let calculo = parseInt(num1) - parseInt(num2);
+
+    const resultado = document.getElementById('resultado');
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    //Setear el valor
+    resultado.innerHTML = calculo;
+
+    
+
+}
+
+function Multiplicar() {
+    let num1 = document.getElementById('numero1').value;
+    let num2 = document.getElementById('numero2').value;
+
+    let calculo = parseInt(num1) * parseInt(num2);
+    let limpiar = 'Resultado';
+
+    const resultado = document.getElementById('resultado');
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    //Setear el valor
+    resultado.innerHTML = calculo;
+}
